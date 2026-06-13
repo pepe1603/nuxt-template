43a3f2ce0f90813a724f53692d3de1944c61ff77
pepe1603 <b180022@unach.mx>
2026-06-13 07:03:46 -0600

    chore(template): initial wizard setup + project generator

--- app/app.config.ts
+++ app/app.config.ts
@@ -1,8 +1,8 @@
 export default defineAppConfig({
   ui: {
     colors: {
-      primary: 'primary',
-      neutral: 'slate'
+      primary: '__PRIMARY_COLOR__',
+      neutral: '__NEUTRAL_COLOR__'
     }
   }
 })
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,37 +1,42 @@
 export default defineNuxtConfig({
+  app: {
+    head: {
+      title: "__APP_TITLE__",
+    },
+  },
   modules: [
-    '@nuxt/eslint',
-    '@nuxt/ui',
-    '@nuxtjs/google-fonts',
-    'nuxt-lucide-icons'
+    "@nuxt/eslint",
+    "@nuxt/ui",
+    "@nuxtjs/google-fonts",
+    "nuxt-lucide-icons",
   ],
 
   devtools: {
-    enabled: false
+    enabled: false,
   },
 
-  css: ['~/assets/css/main.css'],
+  css: ["~/assets/css/main.css"],
 
   routeRules: {
-    '/': { prerender: true }
+    "/": { prerender: true },
   },
 
-  compatibilityDate: '2025-01-15',
+  compatibilityDate: "2025-01-15",
 
   eslint: {
     config: {
       stylistic: {
-        commaDangle: 'never',
-        braceStyle: '1tbs'
-      }
-    }
+        commaDangle: "never",
+        braceStyle: "1tbs",
+      },
+    },
   },
 
   googleFonts: {
     families: {
       DynaPuff: [400, 500, 600, 700],
-      Courgette: [400]
+      Courgette: [400],
     },
-    display: 'swap'
-  }
-})
+    display: "swap",
+  },
+});
